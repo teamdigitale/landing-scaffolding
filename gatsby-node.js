cb62215c6735a5a27b99c04752bbb1021f9005d4
Fabio Bonelli <fb@fabiobonelli.it>
2021-07-21 15:19:32 +0200

    chore: remove old site leftover contents

--- gatsby-node.js
+++ gatsby-node.js
@@ -15,32 +15,3 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
     }
   }
 };
-
-exports.createPages = async ({ actions: { createPage }, graphql }) => {
-  const result = await graphql(`
-    {
-      allMarkdownRemark(filter: { frontmatter: { type: { eq: "news" } } }) {
-        nodes {
-          fields {
-            slug
-          }
-        }
-      }
-    }
-  `);
-
-  if (result.errors) {
-    console.error(result.errors);
-    throw new Error(result.errors);
-  }
-
-  return result.data.allMarkdownRemark.nodes.forEach(({ fields }) => {
-    createPage({
-      path: fields.slug,
-      component: path.resolve(__dirname, 'src/templates/NewsTemplate.js'),
-      context: {
-        slug: fields.slug,
-      },
-    });
-  });
-};
